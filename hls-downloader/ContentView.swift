371ed3064037cb0ff3b7448fe0ab140fcfbcf4e0
Jacky <newbdez33@gmail.com>
2019-11-01 09:53:44 +0900

    Initial Commit

---
+++ hls-downloader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hls-downloader
+//
+//  Created by Jacky on 2019/11/01.
+//  Copyright © 2019 illegal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
